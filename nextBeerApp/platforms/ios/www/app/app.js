f2fde87faaebfc1e049c52f9865a4b477b91d6d9
neillobo <neillobo@outlook.com>
2014-08-01 16:14:44 -0700

    ionic app with HTTP GET/POST requests

--- nextBeerApp/platforms/ios/www/app/app.js
+++ nextBeerApp/platforms/ios/www/app/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic','app.rate','app.services'])
+angular.module('app', ['ionic','app.rate','app.recommend','app.mybeers','app.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -27,24 +27,6 @@ angular.module('app', ['ionic','app.rate','app.services'])
       url: "/app",
       abstract: true,
       templateUrl: "app/app.html"
-    })
-
-    .state('app.recommend', {
-      url: "/recommend",
-      views: {
-        'menuContent' :{
-          templateUrl: "app/recommend/recommend.html"
-        }
-      }
-    })
-
-    .state('app.mybeers', {
-      url: "/mybeers",
-      views: {
-        'menuContent' :{
-          templateUrl: "app/mybeers/mybeers.html"
-        }
-      }
     });
 
   // if none of the above states are matched, use this as the fallback
--- nextBeerApp/www/app/rate/rate.js
+++ nextBeerApp/www/app/rate/rate.js
@@ -17,8 +17,49 @@ angular.module('app.rate', [])
     })
 })
 
-.controller('RateCtrl',function($scope, ShowBeersFactory){
-  $scope.items = ['A','B','C']
-  $scope.showBeer = ShowBeersFactory.showBeers();
+.controller('RateCtrl',function($scope, $http, ShowBeersFactory){
+  
+  $http({
+      method: 'GET', 
+      url: 'http://www.google.com'
+    }).
+    success(function(data, status, headers, config) {
+      console.log(data)
+    
+    }).
+    error(function(data, status, headers, config) {
+      console.log("Error: ",status)
+        $scope.items = [
+        {
+          title: 'Corona',
+          description : "Great"
+        },
+        {
+          title: 'Budlight',
+          description : "Shitty"
+        },
+        {
+          title: 'Blue Moon',
+          description : "Meh"
+        }];
+    });
+
+  $scope.beerClickEvent = function(item){
+    console.log("Item is ",item)
+    
+    delete $http.defaults.headers.common['X-Requested-With'];
+    $http({
+      method: 'POST', 
+      url: 'http://www.google.com',
+       data: item
+    }).
+    success(function(data, status, headers, config) {
+      console.log(data)
+    }).
+    error(function(data, status, headers, config) {
+      console.log("Error: ",status)
+    });
+  };
+  // $scope.showBeer = ShowBeersFactory.showBeers();
 });
 
