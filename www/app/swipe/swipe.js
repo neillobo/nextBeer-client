ab2823bccdf35462e80aad5aa6a296abd2d7e645
C. Moss <caly.moss@gmail.com>
2014-08-04 19:09:42 -0700

    add mybeers queue

--- www/app/swipe/swipe.js
+++ www/app/swipe/swipe.js
@@ -30,34 +30,47 @@ angular.module('app.swipe', ['ionic', 'ngTouch', 'ionic.contrib.ui.cards'])
   }
 })
 
-.controller('CardsCtrl', function($scope, $ionicSwipeCardDelegate,  $rootScope) {
+//needs to accept card from infinite scroll or first card in rec queue
+//swipe right:
+// goes to "my beers" queue
+//swipe left: 
+// gets deleted from rec queue
+//skip: stays in queue
+
+.controller('CardsCtrl', function($scope, $ionicSwipeCardDelegate,  $rootScope, myBeers) {
   $rootScope.accepted = 0;
   $rootScope.rejected = 0;
-  var cardTypes = [
-    { title: 'Swipe down to clear the card', image: 'http://ionicframework.com.s3.amazonaws.com/demos/ionic-contrib-swipecards/pic.png' },
-    { title: 'Where is this?', image: 'http://ionicframework.com.s3.amazonaws.com/demos/ionic-contrib-swipecards/pic.png' },
-    { title: 'What kind of grass is this?', image: 'http://ionicframework.com.s3.amazonaws.com/demos/ionic-contrib-swipecards/pic2.png' },
-    { title: 'What beach is this?', image: 'http://ionicframework.com.s3.amazonaws.com/demos/ionic-contrib-swipecards/pic3.png' },
-    { title: 'What kind of clouds are these?', image: 'http://ionicframework.com.s3.amazonaws.com/demos/ionic-contrib-swipecards/pic4.png' }
-  ];
 
-  $scope.cards = Array.prototype.slice.call(cardTypes, 0, 0);
+  $rootScope.recommendedBeerQueue = [{title : "Budweiser", id: 1, img:"./../img/budweiser.jpg"}, {title : "Corona", id: 2, img:"./../img/corona_logo.jpg"},
+                {title : "Stella" , id: 3, img:"./../img/stella_logo.jpg"},{title : "Natty Light" , id:4, img:"./../img/nattylight_logo.jpg"},
+                {title : "Tecate" , id:5, img:"./../img/tecate_logo.jpg"},{title : "PBR" , id: 6, img:"./../img/pbr_logo.jpg"}
+                ];  
+
+  $scope.cards = $rootScope.recommendedBeerQueue;
 
   $scope.cardSwiped = function(index) {
-    $scope.addCard();
+    //need to send post request with rating of -1 or 1 and get new recommendation
+    //push new recommendation to end of recommendedBeerQueue
+    // $scope.addCard();
   };
 
   $scope.cardDestroyed = function(index) {
     if (this.swipeCard.positive === true) {
       $scope.$root.accepted++;
+     //do post request to add to user's liked beer queue
+     $rootScope.myBeers.push($scope.cards[index]);
+
     } else {
       $scope.$root.rejected++;
     }
     $scope.cards.splice(index, 1);
+    //delete that beer from the recommended beer queue
+    $rootScope.recommendedBeerQueue.splice(index, 1);
   };
 
   $scope.addCard = function() {
-    var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+    //put card into queue from likes
+    var newCard = $rootScope.recommendedBeerQueue[$rootScope.recommendedBeerQueue.length-1];
     newCard.id = Math.random();
     $scope.cards.push(angular.extend({}, newCard));
   }
