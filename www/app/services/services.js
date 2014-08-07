c5e1faaee082f1814abfb996832f1a705ce01925
C. Moss <caly.moss@gmail.com>
2014-08-07 13:29:36 -0700

    send beer ratings to server via post request

--- www/app/services/services.js
+++ www/app/services/services.js
@@ -1,32 +1,47 @@
 angular.module('app.services', [])
-.factory('BeerFactory', function() {
+.factory('BeerFactory', function($http) {
   var beerQueue = [{
-    id: 104,
-    name: "Samuel Adams Boston Lager",
-    image_url: "http://cdn.beeradvocate.com/im/beers/104.jpg"
+    beer_id: 104,
+    beer_name: "Samuel Adams Boston Lager",
+    beer_image_url: "http://cdn.beeradvocate.com/im/beers/104.jpg"
   }, {
-    id: 754,
-    name: "Guinness Draught",
-    image_url: "http://cdn.beeradvocate.com/im/beers/754.jpg"
+    beer_id: 754,
+    beer_name: "Guinness Draught",
+    beer_image_url: "http://cdn.beeradvocate.com/im/beers/754.jpg"
   }, {
-    id: 355,
-    name: "Dead Guy Ale",
-    image_url: "http://cdn.beeradvocate.com/im/beers/355.jpg"
+    beer_id: 355,
+    beer_name: "Dead Guy Ale",
+    beer_image_url: "http://cdn.beeradvocate.com/im/beers/355.jpg"
   }, {
-    id: 1904,
-    name: "Sierra Nevada Celebration Ale",
-    image_url: "http://cdn.beeradvocate.com/im/beers/1904.jpg"
+    beer_id: 1904,
+    beer_name: "Sierra Nevada Celebration Ale",
+    beer_image_url: "http://cdn.beeradvocate.com/im/beers/1904.jpg"
   }, {
-    id: 680,
-    name: "Brooklyn Black Chocolate Stout",
-    image_url: "http://cdn.beeradvocate.com/im/beers/680.jpg"
+    beer_id: 680,
+    beer_name: "Brooklyn Black Chocolate Stout",
+    beer_image_url: "http://cdn.beeradvocate.com/im/beers/680.jpg"
   }, {
-    id: 1212,
-    name: "Blue Moon Belgian White",
-    image_url: "http://cdn.beeradvocate.com/im/beers/1212.jpg"
+    beer_id: 1212,
+    beer_name: "Blue Moon Belgian White",
+    beer_image_url: "http://cdn.beeradvocate.com/im/beers/1212.jpg"
   }];
 
   var mySelectedBeers = [];
+
+  var sendRating = function(ratingObject){
+    return $http({
+      method: 'POST',
+      url: 'http://next-beer.herokuapp.com/api/v2/beer',
+      data: ratingObject
+    });
+  };
+
+  // var getMyBeers = function(beer){
+  //   return $http({
+  //     method: 'GET',
+  //     url: 'http://localhost:5000/api/v1/' + beer.beer_id
+  //   });
+  // };
 
   return {
     beerRecQueue: function() {
@@ -40,7 +55,8 @@ angular.module('app.services', [])
     },
     addToMyBeers: function(beer) {
       mySelectedBeers.push(beer);
-    }
+    },
+    sendRating: sendRating
   };
 })
 
@@ -48,7 +64,7 @@ angular.module('app.services', [])
   var userIdGrabber = function() {
     return $http({
       method: 'POST',
-      url: 'http://localhost:5000/api/v2/user'
+      url: 'http://next-beer.herokuapp.com/api/v2/user'
     });
   };
   var setHeader = function(token) {
