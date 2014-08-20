ca100132a34f85e179f9393d6037b9eff1990744
dhfromkorea <dhllee424@gmail.com>
2014-08-19 18:42:06 -0700

    get ready to show pop up view

--- www/app/detail/detail.js
+++ www/app/detail/detail.js
@@ -1,29 +1,40 @@
 angular.module('app.detail', [])
-.run(function() {
-})
+  .run(function() {})
 
 .config(['$stateProvider',
-    function($stateProvider) {
-      $stateProvider
-        .state('app.detail', {
-          url: "/detail",
-          views: {
-            'menuContent': {
-              templateUrl: "app/detail/detail.html"
-            }
+  function($stateProvider) {
+    $stateProvider
+      .state('app.detail', {
+        url: "/detail",
+        views: {
+          'menuContent': {
+            templateUrl: "app/detail/detail.html"
           }
-        });
-    }
-  ])
+        }
+      });
+  }
+])
 
-.controller('detailCtrl', ['$scope', 'BeerFactory',
-  function($scope, BeerFactory) {
-    $scope.beer = BeerFactory.getSelectedBeer();
+.controller('detailCtrl', ['$scope', 'BeerFactory', '$state',
+  function($scope, BeerFactory, $state) {
+    var beer = BeerFactory.getSelectedBeer();
+    if (!beer) {
+      // in case, there's nothing to show
+      // fall back to recommend view
+      // the trainsition is very slow.
+      // let's find a faster way to do this
+      $state.go('app.recommend');
+    }
+    $scope.beer = beer;
     $scope.addToMyBeers = function() {
-      BeerFactory.addToMyBeers($scope.beer);
+      BeerFactory.addToMyBeers(beer);
+      // should give visual feedback to users
+      // $state.go('app.recommend');
     };
-    $scope.hello = function() {
-      BeerFactory.removeFromMyBeers($scope.beer);
+    $scope.removeFromMyBeers = function() {
+      BeerFactory.removeFromMyBeers(beer);
+      // should show a pop-up for confirmation
+      // $state.go('app.recommend');
     };
   }
 ]);
